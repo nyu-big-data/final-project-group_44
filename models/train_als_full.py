080c68647446a84eb708393bda9fc520ba2961bb
Carolyn Kolaczyk <ck3419@hlog-2.hpc.nyu.edu>
2022-05-13 18:51:59 -0400

    trained and evaluated full als

---
+++ models/train_als_full.py
@@ -0,0 +1,44 @@
+'''Starter Pyspark Script for students to complete for their Lab 3 Assignment.
+Usage:
+    $ spark-submit lab_3_starter_code.py <student_netID>
+'''
+#Use getpass to obtain user netID
+import getpass
+
+# And pyspark.sql to get the spark session
+from pyspark.sql import SparkSession
+from pyspark.ml.recommendation import ALS, ALSModel
+
+
+def main(spark, netID):
+    '''Main routine for Lab Solutions
+    Parameters
+    ----------
+    spark : SparkSession object
+    netID : string, netID of student to find files in HDFS
+    '''
+
+     # Read in training data
+    train = spark.read.csv(f'hdfs:/user/{netID}/pub/ratings_all_train.csv', header='true', schema='index INT, userId INT,movieId INT,rating DOUBLE,timestamp INT')
+
+
+    als = ALS(rank = 30, maxIter=20, regParam=0.1, userCol="userId", itemCol="movieId", ratingCol="rating",\
+                        nonnegative = True, implicitPrefs = True, coldStartStrategy="drop", seed=42)
+
+    model = als.fit(train)
+    model.write().overwrite().save(f"hdfs:/user/{netID}/ALS_model_full_rank30_reg0.1")
+
+
+
+
+# Only enter this block if we're in main
+if __name__ == "__main__":
+
+    # Create the spark session object
+    spark = SparkSession.builder.appName('part1').getOrCreate()
+
+    # Get user netID from the command line
+    netID = getpass.getuser()
+
+    # Call our main routine
+    main(spark, netID)
